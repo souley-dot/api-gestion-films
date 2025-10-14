51d5461d312e69b22c89f425fc18800c38db117c
souley-dot <souleymane010197@gmail.com>
2025-10-14 08:58:05 +0200

    Ajout du fichier de la base de données

---
+++ devoirapi.sql
@@ -0,0 +1,53 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : mar. 14 oct. 2025 à 06:50
+-- Version du serveur : 9.1.0
+-- Version de PHP : 8.3.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `devoirapi`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `film`
+--
+
+DROP TABLE IF EXISTS `film`;
+CREATE TABLE IF NOT EXISTS `film` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nom` varchar(25) NOT NULL,
+  `titre` varchar(50) NOT NULL,
+  `acteur` varchar(25) NOT NULL,
+  `duree` int NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `film`
+--
+
+INSERT INTO `film` (`id`, `nom`, `titre`, `acteur`, `duree`) VALUES
+(1, 'Diallo Souleymane', 'Developppeur ', 'Diallo Souleymae', 1000000000),
+(2, 'Incept', 'Inception', 'Leonardo DiCaprio', 148),
+(3, 'Mamadou', 'oumarou', 'les miserable', 15),
+(4, 'Inception', 'Inception', 'Leonardo DiCaprio', 148);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
